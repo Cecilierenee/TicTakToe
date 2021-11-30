e7982945836e3027a9e78a321fa96a9a5586b017
Cecilie Robb <Cecilie_robb@mymail.eku.edu>
2021-11-30 10:32:42 -0500

    working on script

--- script.js
+++ script.js
@@ -9,13 +9,25 @@ let gameState = ["", "", "", "", "", "", "", "", "",];
 
 //Game status Messages
 const winnerMessage = () => `${currentPlayer} Won!`;
-const tieMessage = () => "Tie Game!";
+const tieMessage = () => `Tie Game!`;
 const activePlayer = () => `${currentPlayer}'s Turn!`;
 
 //Initializing Game status into html
 gameStatus.innerHTML = activePlayer();
 
 //**Game functions**
+
+//Confirming if the game ends in a win or tie. or if the game is still active. Declaring which cell combinations end in a win.
+const winnerConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 //Shows played move and which player turn it is.
 
@@ -29,18 +41,6 @@ function playerChange() {
     currentPlayer = currentPlayer === "X" ? "O" : "X";
     gameStatus.innerHTML = activePlayer();
 }
-
-//Confirming if the game ends in a win or tie. or if the game is still active. Declaring which cell combinations end in a win.
-const winnerConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
 
 //Declaring conditionals for results and what messages to display in the game status area.
 function gameResults() {
@@ -70,6 +70,7 @@ if (roundTie) {
     gameActive = false;
     return;
 }
+activePlayer();
 
 }
 function cellClicked(clickedCellEvent) {
@@ -78,15 +79,16 @@ function cellClicked(clickedCellEvent) {
 
 //Gets the data-cell-index attribute and returns a string value.
 
-const clickedCellIndex = parseInt(clickedCell.getAttribute('.data-cell-index')
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index')
     );
     if (gameState[clickedCellIndex] !== "" || !gameActive) {
         return;
     }
-cellClicked(clickedCell, clickedCellIndex); gameResults();
+cellClicked(clickedCell, clickedCellIndex); 
+gameResults();
 }
 //Setting game back to default settings
-function restGame() {
+function restartGame() {
     gameActive = true;
     currentPlayer = 'X';
     gameState = ["", "", "", "", "", "", "", "", "",];
@@ -96,8 +98,8 @@ function restGame() {
 }
 
 //Event listener for game cells
-document.querySelectorAll('div.cell').forEach(cell => cell.addEventListener('click', cellClicked));
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', cellClicked));
 
-//Restart game button and event listener
-document.querySelector('.reset').addEventListener('click', restGame);
+//Restart game and event listener
+document.querySelector('.reset').addEventListener('click', restartGame);
 
