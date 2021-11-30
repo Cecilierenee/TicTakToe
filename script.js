e822ba5ba03d7bc5d8cf77cee80f13bb70f1d6cf
Cecilie Robb <Cecilie_robb@mymail.eku.edu>
2021-11-30 12:20:01 -0500

    mvp!

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 //Declaring Variables and selecting classes
 const gameStatus = document.querySelector('.status');
-
 let gameActive = true; //stopping point for game
 let currentPlayer = 'X'; //current player
-
 //The empty string in the gameState array track data cells played and if the game is active or not.
 let gameState = ["", "", "", "", "", "", "", "", "",];
 
@@ -14,8 +12,6 @@ const activePlayer = () => `${currentPlayer}'s Turn!`;
 
 //Initializing Game status into html
 gameStatus.innerHTML = activePlayer();
-
-//**Game functions**
 
 //Confirming if the game ends in a win or tie. or if the game is still active. Declaring which cell combinations end in a win.
 const winnerConditions = [
@@ -28,24 +24,21 @@ const winnerConditions = [
     [0, 4, 8],
     [2, 4, 6]
 ];
-
+//Game Functions
 //Shows played move and which player turn it is.
-
 function cellPlayed(clickedCell, clickedCellIndex) {
-gameState[clickedCellIndex] = currentPlayer; 
-clickedCell.innerHTML = currentPlayer;
+    gameState[clickedCellIndex] = currentPlayer; 
+    clickedCell.innerHTML = currentPlayer;
 }
 //Changes active Player and the message in the game status section
-
 function playerChange() {
     currentPlayer = currentPlayer === "X" ? "O" : "X";
     gameStatus.innerHTML = activePlayer();
 }
-
 //Declaring conditionals for results and what messages to display in the game status area.
 function gameResults() {
     let roundWon = false;
-    for(let i = 0; i <= 7; i++) {
+    for (let i = 0; i <= 7; i++) {
         const winCondition = winnerConditions[i];
         let a = gameState[winCondition[0]];
         let b = gameState[winCondition[1]];
@@ -56,35 +49,29 @@ function gameResults() {
         if (a === b && b === c) {
             roundWon = true;
             break;
+        }}
+        if(roundWon) {
+        gameStatus.innerHTML = winnerMessage();
+        gameActive = false;
+        return; //if winning coditions are true, the game ends and displays the winner
         }
-    
-    }
-if(roundWon) {
-    gameStatus.innerHTML = winnerMessage();
-    gameActive = false;
-    return;
-}
-let roundTie = !gameState.includes("");
-if (roundTie) {
-    gameStatus.innerHTML = tieMessage();
-    gameActive = false;
-    return;
-}
+    let roundTie = !gameState.includes("");
+        if (roundTie) {
+        gameStatus.innerHTML = tieMessage();
+        gameActive = false;
+        return; //if the conditions result in a tie, then the tie message is displayed
+        }
 playerChange();
-
-}
+};
 function cellClicked(clickedCellEvent) {
-
     const clickedCell = clickedCellEvent.target;
-
-//Gets the data-cell-index attribute and returns a string value.
-
-   const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
-
-    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+    //Gets the data-cell-index attribute and returns a string value.
+    const clickedCellIndex = parseInt(
+        clickedCell.getAttribute('data-cell-index'));
+        if (gameState[clickedCellIndex] !== "" || !gameActive) {
         return;
-    }
-cellClicked(clickedCell, clickedCellIndex); 
+        }
+cellPlayed(clickedCell, clickedCellIndex); 
 gameResults();
 }
 //Setting game back to default settings
@@ -96,7 +83,6 @@ function restartGame() {
     document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
 
 }
-
 //Event listener for game cells
 document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', cellClicked));
 
