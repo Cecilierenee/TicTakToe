23b1091d6cf98f2a71292a329d834653da639d53
Cecilie Robb <Cecilie_robb@mymail.eku.edu>
2021-12-01 12:34:45 -0500

    first pages commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# TicTakToe
+
+This is the first unit project of the java immersive coding cohort I am in. I this project I will use HTML, CSS, and JavaScript to create a interactive Tic tac toe game. 
+
+# Concepts and methods used
+
+HTML
+  -I added the basic structure needed to display my game. 
+  -Linked both my stylesheet and script to my page.
+  -Used appropriate class/id to fit page context
+
+CSS
+  - I created my gameboard using flex and grid styling.
+  - I used pseudo classes to indicate where a user should click.
+  - I gave each game square its own defined area to ensure user intereacts correctly.
+  - Use google fonts
+
+JavaScript
+  In my script file, I used the following Userstories to guide me during my programing;
+  - As a user, I should be able to start a new tic tac toe game
+  - As a user, I should be able to click on a square to add X first and then O, and so on
+  - As a user, I should be shown a message after each turn for if I win, lose, tie or who's turn it is next
+  - As a user, I should not be able to click the same square twice
+  - As a user, I should be shown a message when I win, lose or tie
+Within my Script I used the following methods
+  -Minipulating the Dom;
+      I gave each of my div elements a data-cell-index. This allowed me to beable to track where the user clicks, if a tile is already in play, and if the comination of tiles meet the conditions for the win loop to run. 
+  -I set several functions to achieve my game play. I had to set fuctions for;
+      Displaying the active player,
+      The pattern in which the players choose squares, 
+      Reseting the game,
+      Which messages to display,
+      Determining if the game is still active or not,
+      Displaying 'X's and 'O's
+  -I used event listeners to tie back into which tile the player clicked, and also one to listen for when the used clicks on the word 'reset'. 
+      
---
+++ script.js
@@ -0,0 +1,91 @@
+//Declaring Variables and selecting classes
+const gameStatus = document.querySelector('.status');//reflects where I update the html to show plays turn
+let gameActive = true; //stopping point for game
+let currentPlayer = 'X'; //current player
+//The empty string in the gameState array track data cells played and if the game is active or not.
+let gameState = ["", "", "", "", "", "", "", "", "",];
+
+//Game status Messages
+const winnerMessage = () => `${currentPlayer} Won!`;
+const tieMessage = () => `Tie Game!`;
+const activePlayer = () => `${currentPlayer}'s Turn!`;
+
+//Initializing Game status into html
+gameStatus.innerHTML = activePlayer();
+
+//Confirming if the game ends in a win or tie. or if the game is still active. Declaring which cell combinations end in a win.
+const winnerConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+//Game Functions
+//Shows played move and which player turn it is.
+function cellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer; 
+    clickedCell.innerHTML = currentPlayer;
+}
+//Changes active Player and the message in the game status section
+function playerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    gameStatus.innerHTML = activePlayer();
+}
+//Declaring conditionals for results and what messages to display in the game status area.
+function gameResults() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winnerConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }}
+        if(roundWon) {
+        gameStatus.innerHTML = winnerMessage();
+        gameActive = false;
+        return; //if winning coditions are true, the game ends and displays the winner
+        }
+    let roundTie = !gameState.includes("");
+        if (roundTie) {
+        gameStatus.innerHTML = tieMessage();
+        gameActive = false;
+        return; //if the conditions result in a tie, then the tie message is displayed
+        }
+playerChange();
+};
+function cellClicked(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    //Gets the data-cell-index attribute and returns a string value.
+    const clickedCellIndex = parseInt(
+        clickedCell.getAttribute('data-cell-index'));
+        if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+        }
+cellPlayed(clickedCell, clickedCellIndex); 
+gameResults();
+}
+//Setting game back to default settings
+function restartGame() {
+    gameActive = true;
+    currentPlayer = 'X';
+    gameState = ["", "", "", "", "", "", "", "", "",];
+    gameStatus.innerHTML = activePlayer();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+
+}
+//Event listener for game cells
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', cellClicked));
+
+//Restart game and event listener
+document.querySelector('.reset').addEventListener('click', restartGame);
+
